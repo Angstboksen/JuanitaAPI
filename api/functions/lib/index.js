4a9186794a965c8544302f52e754f9364afc805e
Hauk Olaussen <olaussen@users.noreply.github.com>
2021-04-09 11:13:57 +0200

    added firebase functions

---
+++ api/functions/lib/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = void 0;
+const functions = require("firebase-functions");
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+});
+//# sourceMappingURL=index.js.map
