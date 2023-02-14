76f63bbe5b3f406cc86543fd10f56dea0da9d42d
Hauk Aleksander Olaussen <olaussen.hauk@gmail.com>
2023-02-14 19:03:28 +0100

    date to string

--- models/search_model.go
+++ models/search_model.go
@@ -1,12 +1,10 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type Search struct {
-	Title     string             `json:"title,omitempty" validate:"required"`
-	Date      primitive.DateTime `json:"date,omitempty" validate:"required"`
-	Requestor Requestor          `json:"requestor,omitempty" validate:"required"`
-	Guild     string             `json:"guild,omitempty" validate:"required"`
-	Duration  int                `json:"duration,omitempty" validate:"required"`
-	Url       string             `json:"url,omitempty" validate:"required"`
+	Title     string    `json:"title,omitempty" validate:"required"`
+	Date      string    `json:"date,omitempty" validate:"required"` // TODO: Change to date
+	Requestor Requestor `json:"requestor,omitempty" validate:"required"`
+	Guild     string    `json:"guild,omitempty" validate:"required"`
+	Duration  int       `json:"duration,omitempty" validate:"required"`
+	Url       string    `json:"url,omitempty" validate:"required"`
 }
